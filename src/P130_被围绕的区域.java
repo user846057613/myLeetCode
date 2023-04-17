839839444ef2de5740227c22c880d3136f3d8ce2
bugmaker <15001727357@163.com>
2023-04-18 00:04:02 +0800

    update

--- src/P130_被围绕的区域.java
+++ src/P130_被围绕的区域.java
@@ -58,4 +58,53 @@ public class P130_被围绕的区域 {
         char[][] a = {{'X','X','X','X'},{'X','O','O','X'},{'X','X','O','X'},{'X','O','X','X'}};
         solve(a);
     }
+
+    public void solve2(char[][] board) {
+        if(board.length == 0) return;
+        int y = board.length;
+        int x = board[0].length;
+
+        boolean[][] isVisit = new boolean[y][x];
+        for(int i = 0; i < y; i++) {
+            for(int j = 0; j < x; j++) {
+                if(board[i][j] == 'O' && (i == 0 || i == y-1 || j == 0|| j == x-1) && !isVisit[i][j]) {
+                    isVisit[i][j] = true;
+                    dfs(y, x ,i , j, isVisit, board);
+                }
+            }
+        }
+
+        fillX(isVisit, board);
+    }
+
+    public void dfs(int maxY, int maxX, int y, int x, boolean[][] isVisit, char[][] board) {
+
+        for(int i = 0; i < dxy.length; i++) {
+            int dy = y + dxy[i][0];
+            int dx = x + dxy[i][1];
+
+            if(dy < 0 || dy >= maxY || dx < 0 || dx >= maxX) {
+                continue;
+            }
+
+            if(isVisit[dy][dx]) {
+                continue;
+            }
+
+            if(board[dy][dx] == 'O' && !isVisit[dy][dx]) {
+                isVisit[dy][dx] = true;
+                dfs(maxY, maxX, dy, dx, isVisit, board);
+            }
+        }
+    }
+
+    public void fillX(boolean[][] isVisit , char[][] board) {
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == 'O' && !isVisit[i][j]) {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
 }
